224338b86fa0259c4c4d1e6508796a78821468e2
Georege <jiahaolovelife@outlook.com>
2020-02-27 20:40:51 +0800

    daily update

--- httpserver/poll/KEPoller.h
+++ httpserver/poll/KEPoller.h
@@ -1,28 +1,56 @@
 #pragma once
 
-#include "../utils/Knoncopyable.h"
+#include <map>
 #include <vector>
-#include "../utils/KTimestamp.h"
+
+#include "KTimestamp.h"
+#include "KEventLoop.h"
+#include "Knoncopyable.h"
 
 struct epoll_event;
 
-namespace kb
+namespace kback
 {
+class Channel;
 
+///
+/// IO Multiplexing with epoll(4).
+///
+/// This class doesn't own the Channel objects.
 class EPoller : noncopyable
 {
-
 public:
-    EPoller();
+    typedef std::vector<Channel *> ChannelList;
+
+    EPoller(EventLoop *loop);
     ~EPoller();
+    /// Polls the I/O events.
+    /// Must be called in the loop thread.
+    Timestamp poll(int timeoutMs, ChannelList *activeChannels);
 
-    Timestamp poll(int timeoutMs, );
+    /// Changes the interested I/O events.
+    /// Must be called in the loop thread.
+    void updateChannel(Channel *channel);
+    /// Remove the channel, when it destructs.
+    /// Must be called in the loop thread.
+    void removeChannel(Channel *channel);
 
-    // 用于储存活动事件
+    void assertInLoopThread() { ownerLoop_->assertInLoopThread(); }
+
+private:
+    static const int kInitEventListSize = 16;
+
+    void fillActiveChannels(int numEvents,
+                            ChannelList *activeChannels) const;
+    void update(int operation, Channel *channel);
+
     typedef std::vector<struct epoll_event> EventList;
+    typedef std::map<int, Channel *> ChannelMap;
 
-
-
+    EventLoop *ownerLoop_;
+    int epollfd_;
+    EventList events_;
+    ChannelMap channels_;
 };
 
-} // namespace kb
+} // namespace kback
